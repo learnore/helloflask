a40c5dbb7a3bd1e8305e757c7a7736fde6a10227
shenli <learnore@163.com>
2021-11-27 15:32:50 +0800

    1、shen.py 用于测试 flask 启动实例 2、app.py 记录相关日志

--- demos/hello/shen.py
+++ demos/hello/shen.py
@@ -8,31 +8,31 @@
 import click
 from flask import Flask
 
-app = Flask(__name__)       # app 或 application 的实例
+shen = Flask(__name__)       # app 或 application 的实例
 
 
 # the minimal Flask application
-@app.route('/')     # shen  注册路由，负责管理 URL 和函数之间的映射[必须以斜杠（\）开始]
+@shen.route('/')     # shen  注册路由，负责管理 URL 和函数之间的映射[必须以斜杠（\）开始]
 def index():        # shen  此函数为，视图函数
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Hello, Shen!</h1>'
 
 
 # bind multiple URL for one view function
-@app.route('/hi')
-@app.route('/hello')
+@shen.route('/hi')
+@shen.route('/hello')
 def say_hello():
     return '<h1>Hello, Flask!</h1>'
 
 
 # dynamic route, URL variable default
-@app.route('/greet', defaults={'name': 'Programmer'})
-@app.route('/greet/<name>')
+@shen.route('/greet', defaults={'name': 'Programmer'})
+@shen.route('/greet/<name>')
 def greet(name):
     return '<h1>Hello, %s!</h1>' % name
 
 
 # custom flask cli command
-@app.cli.command()
+@shen.cli.command()
 def hello():
     """Just say hello."""
     click.echo('Hello, Human!')
